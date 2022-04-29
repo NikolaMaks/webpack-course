1857afad69d032e45a77307f49054ef277667922
Maxim <nikolaenkomak@yandex.ru>
2022-04-29 12:59:36 +0300

    wpack new

---
+++ src/app/index.js
@@ -0,0 +1,11 @@
+import '../public/index.pug';
+import '../style/app.scss';
+console.log("hello!")
+
+
+// const pug = require('pug');
+
+// Compile template.pug, and render a set of data
+// console.log(pug.renderFile('template.pug', {
+//   name: 'Timothy'
+// }));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,46 @@
-const path = require('path');
-
+const path = require('path')
+const miniCss = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src.js',
+    entry: './src/app/index.js',
+    mode: 'development',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-      }
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, '/')
+        }
+    },
+    module: {
+        rules: [
+            {
+           test:/\.(s*)css$/,
+           use: [
+              miniCss.loader,
+              'css-loader',
+              'sass-loader',
+           ]
+        },
+        {
+            test: /\.pug$/,
+            loader: 'pug-loader'
+          },
+          
+        ]
+     },
+     plugins: [
+        new miniCss({
+           filename: 'style.css',
+        }),
+        new HtmlWebpackPlugin({
+            template: PATHS.source + '/index.pug',
+            }),
+     ]
   };
+
+
+  
